6c9962d92e23e04c131a926d0ba551b82a671d95
BONAVENTE, JOHN LUIS <johnluisbonavente@gmail.com>
2026-05-30 10:45:28 +0800

    app.py

--- app.py
+++ app.py
@@ -1369,4 +1369,4 @@ def dashboard_page():
 
 # ---------- Run ----------
 if __name__ == "__main__":
-    uvicorn.run("app:app", host="127.0.0.1", port=8000, reload=False)
+    uvicorn.run("app:app", host="0.0.0.0", port=8000, reload=False)
